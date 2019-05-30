8defba22738541a21e6a1db3d749622c00a3a352
chandanshekar <50917216+chandanshekar@users.noreply.github.com>
2019-05-30 16:46:08 +0530

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+struct node
+{
+unsigned dist[20];
+unsigned from[20];
+} rt[10];
+void main()
+{
+int costmat[20][20],source,desti;
+int nodes,i,j,k,count=0;
+printf("\nEnter the number of nodes : ");
+scanf("%d",&nodes);//Enter the nodes
+printf("\nEnter the cost matrix :\n");
+for(i=0;i<nodes;i++)
+for(j=0;j<nodes;j++) /*for loop construct matrix */
+{
+scanf("%d",&costmat[i][j]);
+costmat[i][i]=0;
+rt[i].dist[j]=costmat[i][j];
+rt[i].from[j]=j;
+}
+for(i=0;i<nodes;i++)
+{
+printf("\n\n For router %d\n",i);
+for(j=0;j<nodes;j++)
+printf("\t\nnode %d via %d Distance %d
+
+",j,rt[i].from[j],rt[i].dist[j]);
+}
+do
+{
+count=0;
+for(i=0;i<nodes;i++) /*i-source node */
+for(j=0;j<nodes;j++) /*j-destination node */
+if(i!=j)
+for(k=0;k<nodes;k++) /*k-intermediate node*/
+if(rt[i].dist[j]>rt[i].dist[k]+rt[k].dist[j])
+{
+rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];/*update cost*/
+rt[i].from[j]=rt[i].from[k];/*update route*/
+count++;
+}
+
+}
+while(count!=0);
+for(i=0; i<nodes; i++)
+{
+printf("\n\n For router %d\n",i+1);
+for(j=0; j<nodes; j++)
+printf("\t\n node%d via %d Distance %d ",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+}
+printf("\n\n");
+}
+
